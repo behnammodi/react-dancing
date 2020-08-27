b4c738826555c4154a8b576ae34f368af2ad86c6
Behnam Mohammadi <itten@live.com>
2020-08-27 21:18:42 +0430

    add eslint

--- src/index.js
+++ src/index.js
@@ -1,43 +1,46 @@
-import React from "react";
+/* eslint-disable react/prop-types, react/display-name */
+import {
+  useRef,
+  forwardRef,
+  createElement,
+  useLayoutEffect,
+  useImperativeHandle,
+} from "react";
 
-const Dancer = React.forwardRef(({ children, ...restProps }, ref) => {
-  const innerRef = React.useRef();
+const Dancer = forwardRef(({ children, ...restProps }, ref) => {
+  const innerRef = useRef();
 
-  React.useImperativeHandle(ref, () => {
-    const target = innerRef.current;
+  useImperativeHandle(ref, () => ({
+    setStyle: (style) => {
+      if (!style) return;
 
-    return {
-      setStyle: (style) => {
-        if (!style) return;
+      const keys = Object.keys(style);
+      keys.forEach((key) => {
+        innerRef.current.style[key] = style[key];
+      });
+    },
+    setDuration: (duration = "0.2s") => {
+      innerRef.current.style.transitionDuration = duration;
+    },
+    setTimingFunction: (timingFunction) => {
+      if (!timingFunction) return;
 
-        const keys = Object.keys(style);
-        keys.forEach((key) => {
-          target.style[key] = style[key];
-        });
-      },
-      setDuration: (duration = "0.2s") => {
-        target.style.transitionDuration = duration;
-      },
-      setTimingFunction: (timingFunction) => {
-        if (!timingFunction) return;
+      innerRef.current.style.transitionTimingFunction = timingFunction;
+    },
+    setDelay: (delay) => {
+      if (!delay) return;
 
-        target.style.transitionTimingFunction = timingFunction;
-      },
-      setDelay: (delay) => {
-        if (!delay) return;
+      innerRef.current.style.transitionDelay = delay;
+    },
+  }));
 
-        target.style.transitionDelay = delay;
-      },
-    };
-  });
-
-  return React.createElement("div", { ...restProps, ref: innerRef }, children);
+  return createElement("div", { ...restProps, ref: innerRef }, children);
 });
 
 const useDancer = ({ defaultStyle, duration, timingFunction, delay } = {}) => {
-  const ref = React.useRef();
+  const ref = useRef();
 
-  React.useLayoutEffect(() => {
+  useLayoutEffect(() => {
     ref.current.setStyle(defaultStyle);
     ref.current.setDuration(duration);
     ref.current.setTimingFunction(timingFunction);
