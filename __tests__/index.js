3cd2b03675b07948d01f017f5087d7a61702cf85
Behnam Mohammadi <itten@live.com>
2020-09-06 00:28:52 +0430

    add new test case

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,7 +1,11 @@
 /* eslint-disable no-undef */
-import React from 'react';
+import '@babel/polyfill';
+import React, { useEffect } from 'react';
+import ReactDOM from 'react-dom';
 import { Dancer, useDancer } from '../src';
 import { create } from 'react-test-renderer';
+
+const delay = (timeout) => new Promise((done) => setTimeout(done, timeout));
 
 test('Snapshot a simple <Dancer />', () => {
   function App() {
@@ -25,4 +29,35 @@ test('Snapshot a <Dancer /> with some props', () => {
   const component = create(<App />);
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+test('Check style a <Dancer />', async () => {
+  function App() {
+    const [ref, play] = useDancer();
+
+    useEffect(() => {
+      play({ transform: 'translateX(100px)' });
+    }, []);
+
+    return (
+      <Dancer ref={ref} className="a" id="b" style={{ margin: 100 }}></Dancer>
+    );
+  }
+  const root = document.createElement('div');
+
+  ReactDOM.render(<App />, root);
+
+  const dancer = root.querySelector('div');
+
+  /**
+   * defualt style
+   */
+  expect(dancer.style.transitionDuration).toBe('0.2s');
+
+  await delay(10);
+
+  /**
+   * style after run play
+   */
+  expect(dancer.style.transform).toBe('translateX(100px)');
 });
